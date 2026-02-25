56754beca3555f97b9ac84f085edde2c41e4f557
KristianMoncada <118566826+KristianMoncada@users.noreply.github.com>
2026-02-24 22:38:35 -0500

    Created Player Class

---
+++ src/main/java/com/pl/premier_zone/Player/Player.java
@@ -0,0 +1,10 @@
+package main.java.com.pl.premier_zone.Player;
+
+import jakarta.persistance.Entity;
+import jakarta.persistance.Table;
+
+@Entity
+@Table(name = prem_stats)
+public class Player {
+    
+}
